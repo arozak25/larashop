993fde5642f16943fc1cb8e87ac6a4a41f5dcc05
arozak25 <arozak25@gmail.com>
2017-08-02 10:36:32 +0700

    eloquent orm basic

--- app/Category.php
+++ app/Category.php
@@ -8,7 +8,7 @@ class Category extends Model
 {
     protected $primaryKey = 'id';
     protected $table = 'categories';
-
+    protected $fillable = array('name', 'created_at_ip', 'updated_at_ip');
     //Add this property if you want to turn down the created_at and updated_at column
     //public $timestamps = false;
 }
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,45 @@ Route::get('blade', function () {
     $drinks = array('Vodka','Gin','Brandy');
     return view('page',array('name' => 'The Raven','day' => 'Friday','drinks' => $drinks));
 });
+
+//ELOQUENT ORM INSERT
+Route::get('/insert', function() {
+    App\Category::create(array('name' => 'Music'));
+    return 'category added';
+});
+
+//ELOQUENT ORM READ
+Route::get('/read', function() {
+    $category = new App\Category();
+    
+    $data = $category->all(array('name','id'));
+
+    foreach ($data as $list) {
+        echo $list->id . ' ' . $list->name . '<br/>' ;
+    }
+});
+
+//ELOQUENT ORM UPDATE
+Route::get('/update', function() {
+    $category = App\Category::find(6);
+    $category->name = 'HEAVY METAL';
+    $category->save();
+    
+    $data = $category->all(array('name','id'));
+
+    foreach ($data as $list) {
+        echo $list->id . ' ' . $list->name . '<br />';
+    }
+});
+
+//ELOQUENT ORM DELETE
+Route::get('/delete', function() {
+    $category = App\Category::find(5);
+    $category->delete();
+    
+    $data = $category->all(array('name','id'));
+
+    foreach ($data as $list) {
+        echo $list->id . ' ' . $list->name . '<br />';
+    }
+});
